c88edf128c2a703dd0a2c234287f8571a81073da
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-03-07 23:01:40 +0100

    feat: draw lines on canvas

---
+++ app/src/main/java/com/example/snakemobile/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.example.snakemobile.utils;
+
+public class Constants {
+  public static final double MAX_UPS = 30.0;
+  public static final double UPS_PERIOD = 1E+3 / MAX_UPS;
+
+  public static final float NUM_VERTICAL_LINES = 18.0f;
+  public static final float NUM_HORIZONTAL_LINES = 9.0f;
+}
